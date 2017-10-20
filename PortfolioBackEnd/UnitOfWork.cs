f8df1ef5ce8bdd2e35ee31927f9769692b540f44
Mathilde <ceccaroli.m@gmail.com>
2017-10-20 13:02:54 +1000

    [#11] introduce UnitOfWork pattern

---
+++ PortfolioBackEnd/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioBackEnd
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private PortfolioDbContext _dbContext;
+
+        public UnitOfWork(PortfolioDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Commit()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
